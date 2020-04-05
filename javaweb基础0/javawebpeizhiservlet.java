02fb1c0797cb28cfbaa933e5d0967ca15a020176
MissData-bits <1491331726@qq.com>
2020-04-05 18:01:51 +0800

    1718064054

---
+++ javaweb基础0/javawebpeizhiservlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class javawebpeizhiservlet extends HttpServlet {
+    //init method
+    public void init() throws ServletException {}
+    //handle get request
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+    //handle post request
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+    //handle put request
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    //handle delete request
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    //destroy
+    public void destroy() {}
+}
